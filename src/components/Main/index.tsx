b35bb6fd25c556187f1262f71bcf38b2da1f17ec
Tomás Lopes <tomas050302@gmail.com>
2020-08-18 19:01:45 +0100

    feat(main): added main elements

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  PinsRow,
+  CardsContainer,
+  Card,
+  ContributionsContainer,
+  ContributionGraph,
+  ContributionYears,
+  Year,
+} from './styles';
 
 const Main: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <PinsRow>
+        <span>Pinned</span>
+
+        <span>Customize your pins</span>
+      </PinsRow>
+
+      <CardsContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardsContainer>
+
+      <ContributionsContainer>
+        <ContributionGraph></ContributionGraph>
+
+        <ContributionYears>
+          <Year>2020</Year>
+          <Year>2019</Year>
+        </ContributionYears>
+      </ContributionsContainer>
+    </Container>
+  );
 };
 
 export default Main;
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+
+  flex-direction: column;
+`;
+
+export const PinsRow = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+
+  > span {
+    color: var(--text);
+  }
+`;
+
+export const CardsContainer = styled.div``;
+
+export const Card = styled.div``;
+
+export const ContributionsContainer = styled.div`
+  display: flex;
+`;
+
+export const ContributionGraph = styled.div``;
+
+export const ContributionYears = styled.div``;
+
+export const Year = styled.div``;
